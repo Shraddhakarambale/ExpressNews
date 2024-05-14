aead3830882def5a1aa86d704fd6c2f860ba9929
SHRADDHA SANTOSH Vaddebail <shraddhakarambale@gmail.com>
2024-05-14 15:43:54 +0200

    I created Article,Category,Status,ArticleCategoryLink classes in model folder.please review

---
+++ ExpressNews/Models/Database/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExpressNews.Models.Database
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        public virtual ICollection<ArticleCategoryLink> ArticleCategorys { get; set; }
+    }
+}
---
+++ ExpressNews/Models/Database/Status.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExpressNews.Models.Database
+{
+    public class Status
+    {
+       public int Id { get; set; }
+       
+        [Required]
+        [StringLength(100)]
+        public string StausName { get; set; }
+        public virtual Article Article { get; set; }
+
+    }
+}
