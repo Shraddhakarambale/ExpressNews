55ba0bcec49033416bfa16259cc9c2599bb51f5f
Dell <Dell@DESKTOP-IB4DAQF>
2024-05-17 07:59:03 +0200

    sub

---
+++ ExpressNews/Controllers/Subscription.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExpressNews.Controllers
+{
+    public class Subscription : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
