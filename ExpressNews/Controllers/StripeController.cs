29e4f14f2b219784d1b9d09c5223c40457b48100
Dell <Dell@DESKTOP-IB4DAQF>
2024-07-01 14:10:54 +0200

    email

--- ExpressNews/Controllers/StripeController.cs
+++ ExpressNews/Controllers/StripeController.cs
@@ -5,9 +5,13 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
 using Newtonsoft.Json;
 using Stripe;
+using Stripe.FinancialConnections;
+using System.Numerics;
 using System.Security.AccessControl;
+using System.Threading.Tasks;
 
 namespace ExpressNews.Controllers
 {
@@ -81,8 +85,24 @@ namespace ExpressNews.Controllers
 
             _subscriptionService.UpadateSubscription(modelObj);
 
-            _emailSender.SendEmailAsync(model.Email, "Confirm your payment",
-                        $"Thank you for your payment! Your transaction has been completed successfully. <a href='https://expressnews.azurewebsites.net/'>Express News</a>.");
+            var html = "<html>";
+            html += "<body>";
+            html += "<div class='container'>";  
+            html += "<h1> Payment Confirmation</h1>";
+            html += "<p>Hi Customer,</p>";
+            html += "<p>Thank you for your payment! Your transaction was successful.</p>";
+            html += $"<p>Subscriptin Type: <strong>{model.SubType}</strong></p>";
+            html += $"<p>Expiration Date: <strong>{model.ExpirationDate}</strong></p>";
+            html += $"<p>Amount Paid: <strong> SEK{model.Amount}</strong></p>";
+            html += "<a href='https://expressnews.azurewebsites.net/' class='btn btn-home mt-3 fw-bold'> Go to Homepage</a>";
+            html += "<footer>";
+            html += "<p>If you have any questions, please contact our support team or email us : <a href='https://expressnews.azurewebsites.net/Home/ContactUs' class='btn btn-home mt-3 fw-bold'> Contact Us</a>.</p>";
+            html += "</footer>";
+            html += "</div>";
+            html += "</body>";
+           html += " </html>";
+
+            _emailSender.SendEmailAsync(model.Email, "Confirm your payment", html);
 
             return View("ChargeConfirmation");
         }
