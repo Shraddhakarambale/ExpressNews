026bf0dcc19603487f0fb227495075c6a9ee9669
Dell <Dell@DESKTOP-IB4DAQF>
2024-07-01 14:53:42 +0200

    Subscrib email

--- ExpressNews/Services/NewsletterServices.cs
+++ ExpressNews/Services/NewsletterServices.cs
@@ -3,6 +3,7 @@ using ExpressNews.Data;
 using ExpressNews.Models;
 using ExpressNews.Models.Database;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 namespace ExpressNews.Services
 {
     public class NewsletterServices: INewsletterService
@@ -11,10 +12,12 @@ namespace ExpressNews.Services
         private readonly IConfiguration _configuration;
         private readonly IHttpContextAccessor _httpContextAccessor;
         private readonly UserManager<User> _userManagement;
-        public NewsletterServices (ApplicationDbContext db, IConfiguration configuration)
+        private readonly IEmailSender _emailSender;
+        public NewsletterServices (ApplicationDbContext db, IConfiguration configuration, IEmailSender emailSender)
         {
             _db = db;
             _configuration = configuration;
+            _emailSender = emailSender;
         }
 
         public List<NewsLetter> GetNewsletter()
@@ -39,12 +42,36 @@ namespace ExpressNews.Services
 
                 _db.NewsLetters.Update(newsletter2);
                 _db.SaveChanges();
+
+                
             }
             else {
                
                 _db.NewsLetters.Add(newsLetter);
                 _db.SaveChanges();
             }
+
+            var html = "";
+            html += "<html>";
+            html += "<body>";
+            html += "<div class='container'>";
+            html += "<div class='header'>";
+            html += "<h1>Thank You for Subscribing!</h1>";
+            html += "</div>";
+            html += "<div class='content'>";
+            html += "<h2>Hi there,</h2>";
+            html += "<p>Thank you for subscribing to our newsletter! We're excited to have you on board. You will now receive the latest updates, news, and special offers directly in your inbox.</p>";
+            html += "<p>If you have any questions or need further assistance, feel free to<a href='mailto:expressnewscontact@gmail.com'> contact us</a>.</p>";
+            html += "<a href ='https://expressnews.azurewebsites.net/' class='button'>Visit our website</a>";
+            html += "</div>";
+            html += "<div class='footer'>";
+            html += "<p>&copy; 2024 Express News.All rights reserved.</p>";
+            html += "</div>";
+            html += "</div>";
+            html += "</body>";
+            html += "</html>";
+
+            _emailSender.SendEmailAsync(newsLetter.EmailAddress, "Confirm your Subscribing!", html);
         }
         public NewsLetter GetNewsletterCategoryByUser(string userName)
         {
